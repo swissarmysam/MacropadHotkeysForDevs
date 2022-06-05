5fb762379fdbdd70838a9e188c024f5169e368b5
Sam R <33488943+swissarmysam@users.noreply.github.com>
2022-06-05 12:31:31 +0100

    Label update

--- macros/win-general.py
+++ macros/win-general.py
@@ -5,7 +5,7 @@
 from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
 
 app = {                # REQUIRED dict, must be named 'app'
-    'name' : 'Windows General', # Application name
+    'name' : 'Windows Gen.', # Application name
     'macros' : [       # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
